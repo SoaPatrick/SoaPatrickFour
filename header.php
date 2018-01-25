79132189ffdb5847afc3945a4c32c1dc2e03f3fd
Patrick Leisinger <pleisinger@tocco.ch>
2018-01-25 06:59:01 +0100

    add favicon

--- header.php
+++ header.php
@@ -19,6 +19,14 @@
 	<script>
 		FontAwesomeConfig = { searchPseudoElements: true };
 	</script>
+	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/soapatrickfour/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/soapatrickfour/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/soapatrickfour/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/wp-content/themes/soapatrickfour/favicon/manifest.json">
+	<link rel="mask-icon" href="/wp-content/themes/soapatrickfour/favicon/safari-pinned-tab.svg" color="#28aae2">
+	<link rel="shortcut icon" href="/wp-content/themes/soapatrickfour/favicon/favicon.ico">
+	<meta name="msapplication-config" content="/wp-content/themes/soapatrickfour/favicon/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">	
 	<?php wp_head(); ?>
 </head>
 
