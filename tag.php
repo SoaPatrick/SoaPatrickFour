3e41c3e65d2e042aa70d71ec212890ecd6ca769d
Patrick Leisinger <patrick.leisinger@gmail.com>
2018-03-10 11:22:54 +0100

    improve storage and tag archive view

---
+++ tag.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package soapatrickthree
+ */
+
+get_header(); ?>
+
+    <div class="site-content blog-post-archive-list">
+	    <div class="container">
+		
+		<?php
+		if ( have_posts() ) : ?>	
+		
+			<header class="page-header">
+				<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/"><i class="fal fa-tags"></i> Tags:</a> <span class="no-wrap"><?php the_archive_title();?></span></h1>
+			</header>		
+
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			endwhile;
+			
+			the_posts_navigation();			
+
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif; ?>
+
+		</div>
+	</div>
+
+<?php
+get_footer();
